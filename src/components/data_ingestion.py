28dc5fae6d6d937306f74691337510a77070d482
itsfarman-07 <skmdfarman49@gmail.com>
2023-08-07 17:01:19 +0530

    trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ sys.path.insert(0, 'src/components')
 from data_transformation import DataTransformation
 from data_transformation import DataTransformationConfig
 
+from model_trainer import ModelTrainerConfig
+from model_trainer import ModelTrainer
+
 
 from dataclasses import dataclass
 import pandas as pd
@@ -55,4 +58,8 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
+
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
